b86787389e356981e4ab980077931ae65cce330d
Oronno Akter Usha <ushaoronno1@gmail.com>
2024-03-22 21:40:42 +0600

    added components

---
+++ src/component/Users/Uesrs.jsx
@@ -0,0 +1,28 @@
+import { useLoaderData } from "react-router-dom";
+import User from "../User/User";
+import './Uses.css'
+
+
+const Uesrs = () => {
+    const users = useLoaderData();
+
+    // state --> data
+    // state --> loader
+    // use effect
+    // fetch --> state set --> set state
+
+    console.log(users);
+    return (
+        <div >
+            <h2>Our Users: {users.length}</h2>
+            <p>Fantastic Users</p>
+            <div className="users">
+            {
+                users.map(user => <User key={user.id} data={user}></User>)
+            }
+            </div>
+        </div>
+    );
+};
+
+export default Uesrs;
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,41 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from './component/Home/Home.jsx';
+import About from './component/About/About.jsx';
+import Contact from './component/Contact/Contact.jsx';
+import Uesrs from './component/Users/Uesrs.jsx';
+
+const  router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Home></Home>,
+    children:[
+      {
+        path:'/about',
+        element: <About></About>
+      },
+      {
+        path: '/contact',
+        element: <Contact></Contact>
+      },
+      {
+        path:'/users',
+        loader: () => fetch('https://jsonplaceholder.typicode.com/users'),
+        element: <Uesrs></Uesrs>
+      }
+    ]
+  }
+
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
